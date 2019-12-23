d294cac38ee09830794675857fead3092ca3fb51
General Grievous (Lucas) <42550065+Shaw358@users.noreply.github.com>
2019-12-23 02:03:34 +0100

    shieeet

--- Assets/Scripts/Driving.cs
+++ Assets/Scripts/Driving.cs
@@ -5,25 +5,26 @@ using TMPro;
 
 public class Driving : MonoBehaviour
 {
-    #region Debugging
+    #region Debugging variables
     private TextMeshProUGUI tmpro;
     #endregion
 
-    #region speed/Rigidbodies
+    #region Speed/Rigidbodies variables
     private Rigidbody rb;
-    private Vector3 m_EulerAngleVelocity;
+    private Vector3 rotationDirection;
     [SerializeField] private int speed;
     [SerializeField] private int maxSpeed;
-    private float generalTurnSpeed;
     [SerializeField] private int turnSpeed;
     #endregion
 
+    //defining unity variables such as finding components of gameobjects
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         tmpro = GameObject.Find("SpeedText").GetComponent<TextMeshProUGUI>();
     }
 
+    //defining variables
     private void Start()
     {
         turnSpeed = 60;
@@ -31,33 +32,37 @@ public class Driving : MonoBehaviour
         maxSpeed = 2100;
     }
 
+    //working with FixedUpdate due to physics
     void FixedUpdate()
     {
         #region move left & right
         if (Input.GetKey(KeyCode.A))
         {
-            m_EulerAngleVelocity = new Vector3(0, -turnSpeed, 0);
+            rotationDirection = new Vector3(0, -turnSpeed, 0);
 
-            Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
+            Quaternion deltaRotation = Quaternion.Euler(rotationDirection * Time.deltaTime);
             rb.MoveRotation(rb.rotation * deltaRotation);
             print(deltaRotation);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            m_EulerAngleVelocity = new Vector3(0, turnSpeed, 0);
+            rotationDirection = new Vector3(0, turnSpeed, 0);
 
-            Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
+            Quaternion deltaRotation = Quaternion.Euler(rotationDirection * Time.deltaTime);
             rb.MoveRotation(rb.rotation * deltaRotation);
             print(deltaRotation);
         }
         #endregion
 
+        //Dis shit for debugging only
         tmpro.text = rb.velocity.magnitude.ToString();
 
         #region move forward & backward
         if (Input.GetKey(KeyCode.W))
         {
             rb.AddForce(new Vector3(transform.forward.x, 0, transform.forward.z) * speed);
+
+            //limits the speed of the kart
             if(rb.velocity.magnitude >= maxSpeed)
             {
                 rb.velocity *= 0.99f;
