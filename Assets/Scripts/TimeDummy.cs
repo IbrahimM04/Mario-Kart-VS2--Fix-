ff456c8996ded53e3619c3376993cb8c7e648fe2
IbrahimM04 <43440821+IbrahimM04@users.noreply.github.com>
2020-01-12 16:01:38 +0100

    Time Tracker (Done)

---
+++ Assets/Scripts/TimeDummy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeDummy : MonoBehaviour {
+    [SerializeField] Text TimeStamp;
+    [SerializeField] bool playing;
+    private float Timer;
+
+    private void Start() {
+        playing = true;
+    }
+
+    private void Update() {
+        if (playing == true) {
+            Timer += Time.deltaTime;
+            int minutes = Mathf.FloorToInt(Timer / 60f);
+            int seconds = Mathf.FloorToInt(Timer % 60f);
+            int milliseconds = Mathf.FloorToInt((Timer * 100f) % 100f);
+            TimeStamp.text = "Time " + minutes.ToString("00") + ":" + seconds.ToString("00") + ":" + milliseconds.ToString("000");
+        }
+    }
+}
