91540bffa208398cc2d013278312fa7c3155256c
ZeroTwoWaifu <tobias2002@live.nl>
2020-01-13 08:55:37 +0100

    inventory block done

---
+++ Assets/Scripts/BlockBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockBehaviour : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+
+    void Update()
+    {
+
+
+        transform.position = new Vector3(0, Mathf.Sin(Time.time * 3) / Mathf.PI / 7, 0);
+    }
+}
+    
