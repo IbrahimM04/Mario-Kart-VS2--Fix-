1f46fdff2d4847be8a793c606023964583e92f91
IbrahimM04 <43440821+IbrahimM04@users.noreply.github.com>
2020-01-16 11:32:03 +0100

    ??

--- Assets/Scripts/TimeDummy.cs
+++ Assets/Scripts/TimeDummy.cs
@@ -7,18 +7,25 @@ public class TimeDummy : MonoBehaviour {
     [SerializeField] Text TimeStamp;
     [SerializeField] bool playing;
     private float Timer;
+    int minutes;
+    int seconds;
+    int milliseconds;
 
     private void Start() {
-        playing = true;
+        playing = false;
     }
 
     private void Update() {
         if (playing == true) {
             Timer += Time.deltaTime;
-            int minutes = Mathf.FloorToInt(Timer / 60f);
-            int seconds = Mathf.FloorToInt(Timer % 60f);
-            int milliseconds = Mathf.FloorToInt((Timer * 100f) % 100f);
+            minutes = Mathf.FloorToInt(Timer / 60f);
+            seconds = Mathf.FloorToInt(Timer % 60f);
+            milliseconds = Mathf.FloorToInt((Timer * 100f) % 100f);
             TimeStamp.text = "Time " + minutes.ToString("00") + ":" + seconds.ToString("00") + ":" + milliseconds.ToString("000");
         }
+        if (Input.GetKeyDown(KeyCode.R)) {
+            Timer = 0;
+        }
+   
     }
 }
