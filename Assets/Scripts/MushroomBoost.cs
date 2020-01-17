5ddb5eadd7c281f9f1d5d79e3040702823b5e718
ZeroTwoWaifu <tobias2002@live.nl>
2020-01-17 08:51:24 +0100

    Mushroom Starter
    Moet nog de boost in komen

---
+++ Assets/Scripts/MushroomBoost.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomBoost : MonoBehaviour
+{
+
+    [SerializeField]private int itemCount; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(itemCount > 0)
+            {
+                UseItem();
+            }
+            else if(itemCount == 0)
+            {
+
+            }
+        }
+    }
+
+    void UseItem()
+    {
+        itemCount -= 1;
+    }
+}
